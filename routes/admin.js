fddd6b86b3a2385075197def38e7ffb32eb077db
Varsányi Barnabás <varsanyib0125@gmail.com>
2025-11-24 23:26:51 +0100

    Add admin user list view and route
    Replaced the admin route response with a rendered admin.ejs template showing a table of registered users. Added a new admin.ejs view to display user details for administrators.

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
- res.send('This is a protected route');
+const {query} = require('../module/db');
+
+router.get('/', async function(req, res, next) {
+    const users = await query('SELECT id, name, username, isAdmin, createdAt, modifiedAt FROM users;');
+
+    res.render('admin', { users, user: req.session.user});
 });
 
 module.exports = router;
